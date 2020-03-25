6b10eb0ec5047bed721005178a6dd0cf765fcbe6
JeG99 <a00824494@itesm.mx>
2020-03-25 16:33:13 -0600

    add animations for player, allies and enemies

--- src/videogame/Assets.java
+++ src/videogame/Assets.java
@@ -4,9 +4,9 @@ import java.awt.image.BufferedImage;
 
 /**
  * Assets
- * 
+ *
  * Clase para administrar y cargar recursos de multimedia.
- * 
+ *
  * @author Alam Sánchez
  */
 public class Assets {
@@ -20,9 +20,26 @@ public class Assets {
     public static SoundClip score;              // sonido de colisión con aliado
     public static SoundClip gameOverSound;      // sonido de game over
 
+    // for animations loading
+    public static BufferedImage playerSprites; // to store the player sprites
+    public static BufferedImage enemySprites;  // to store the enemy sprites
+    public static BufferedImage allySprites;   // to store the ally sprites
+
+    // for player animations
+    public static BufferedImage playerUp[];    // pictures to go up
+    public static BufferedImage playerLeft[];  // pictures to go left
+    public static BufferedImage playerRight[]; // pictures to go right
+    public static BufferedImage playerDown[];  // pictures to go down
+
+    //for enemy animations
+    public static BufferedImage enemyLeft[];   // pictures to go left
+
+    //for ally animations
+    public static BufferedImage allyRight[];   // pictures to go right
+
     /**
      * init
-     * 
+     *
      * Método para cargar los recursos
      */
     public static void init() {
@@ -34,6 +51,43 @@ public class Assets {
         hit = new SoundClip("/sounds/hit.wav");
         score = new SoundClip("/sounds/score.wav");
         gameOverSound = new SoundClip("/sounds/gameOver.wav");
+
+        // loading player sprites
+        playerSprites = ImageLoader.loadImage("/images/PlayerSS.png");
+        Spritesheet playerSpritesheet = new Spritesheet(playerSprites);
+
+        playerUp = new BufferedImage[9];
+        playerDown = new BufferedImage[9];
+        playerRight = new BufferedImage[9];
+        playerLeft = new BufferedImage[9];
+
+        for (int i = 0; i < 9; i++) {
+            playerUp[i] = playerSpritesheet.crop(i * 64, 0, 64, 64);
+            playerLeft[i] = playerSpritesheet.crop(i * 64, 64, 64, 64);
+            playerDown[i] = playerSpritesheet.crop(i * 64, 128, 64, 64);
+            playerRight[i] = playerSpritesheet.crop(i * 64, 192, 64, 64);
+        }
+
+        //loading enemy sprites
+        enemySprites = ImageLoader.loadImage("/images/PlayerSS.png");
+        Spritesheet enemySpritesheet = new Spritesheet(enemySprites);
+
+        enemyLeft = new BufferedImage[9];
+
+        for (int i = 0; i < 9; i++) {
+            enemyLeft[i] = enemySpritesheet.crop(i * 64, 64, 64, 64);
+        }
+
+        //loading enemy sprites
+        allySprites = ImageLoader.loadImage("/images/PlayerSS.png");
+        Spritesheet allySpritesheet = new Spritesheet(allySprites);
+
+        allyRight = new BufferedImage[9];
+
+        for (int i = 0; i < 9; i++) {
+            allyRight[i] = allySpritesheet.crop(i * 64, 192, 64, 64);
+        }
+
     }
 
 }
--- src/videogame/Enemy.java
+++ src/videogame/Enemy.java
@@ -4,18 +4,22 @@ import java.awt.Graphics;
 
 /**
  * Enemy
- * 
+ *
  * Clase que representa a un enemigo en el juego
+ *
  * @author Andrés Alam Sánchez Torres
  */
-public class Enemy extends Item{
+public class Enemy extends Item {
+
     private Game game;
-    
+    private Animation animationLeft;
+    private Animation currentAnimation;
+
     /**
      * Enemy
-     * 
+     *
      * Constructor de la clase
-     * 
+     *
      * @param width es el ancho del enemigo
      * @param height es el alto del enemigo
      * @param game es la referencia al juego
@@ -23,12 +27,14 @@ public class Enemy extends Item{
     public Enemy(int width, int height, Game game) {
         super(0, 0, width, height);
         this.game = game;
+        this.animationLeft = new Animation(Assets.enemyLeft, 100);
+        this.currentAnimation = animationLeft; // for future use
         setNewPosition();
     }
 
     /**
      * setNewPosition
-     * 
+     *
      * Da una posición inicial
      */
     public void setNewPosition() {
@@ -36,19 +42,22 @@ public class Enemy extends Item{
         setY((int) (Math.random() * game.getHeight()));
     }
 
-
     @Override
     public void tick() {
         int moveDistance = (int) (Math.random() * 3) + 3;
         setX(getX() - moveDistance);
+        currentAnimation = animationLeft;
+        currentAnimation.tick();
 
-        if (getX() < - getWidth()) {
+        if (getX() < -getWidth()) {
             setNewPosition();
+
         }
     }
 
     @Override
     public void render(Graphics g) {
-        g.drawImage(Assets.enemy, getX(), getY(), getWidth(), getHeight(), null);
+        g.drawImage(currentAnimation.getCurrentFrame(), getX(), getY(), getWidth(), getHeight(), null);
+        //g.drawImage(Assets.enemy, getX(), getY(), getWidth(), getHeight(), null);
     }
 }
